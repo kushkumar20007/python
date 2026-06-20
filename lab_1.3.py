936e2bfaaba938d6e96e710b0692b0d3f8e245bf
khush <kushabcd802312@gmail.com>
2026-06-20 12:45:17 +0530

    done

---
+++ lab_1.3.py
@@ -0,0 +1,18 @@
+#demonstrate the use of type casting constructors
+#Take the input from the user with string. 
+# Convert string into an int, float and bool 
+# Print values and types 
+a = input("Enter the name: ") 
+b = int(a) 
+c = float(a) 
+d = bool(a) 
+print(a)
+print(type(a)) 
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))  
+
+0
